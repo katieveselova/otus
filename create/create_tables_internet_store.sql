cf02e4f954d028668632ac30e2a28d1831051da7
katieveselova <elena116116@bk.ru>
2025-11-06 00:51:49 +0300

    Update create_tables_internet_store.sql

--- create/create_tables_internet_store.sql
+++ create/create_tables_internet_store.sql
@@ -11,4 +11,17 @@ create table customers (
   -- status: 1 = активен, 0 = неактивен, 2 = заблокирован
 );
 
+create table if not exists customer_addresses (
+  address_id     bigint generated always as identity primary key,
+  customer_id    bigint not null,
+  is_default     boolean not null default false,
+  receiver_name  text not null,
+  country_code   char(2) not null,
+  city           text not null,
+  addressline    text not null,
+  created_at     timestamptz not null default now(),
+  constraint fk_customer_addresses_customer
+    foreign key (customer_id) references public.customers(customer_id) on delete cascade,
+  constraint chk_country_code_iso2_upper check (country_code ~ '^[A-Z]{2}$')
+);
 
